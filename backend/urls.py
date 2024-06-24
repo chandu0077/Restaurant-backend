fb66a560a9872e2c8a0f1562cd9010f0353be2d2
chandu0077 <chandu.dsc77@gmail.com>
2024-06-24 15:32:54 +0530

    modified requirments file

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from backend.backend import settings
 from restaurant import views
 from rest_framework import routers
 
